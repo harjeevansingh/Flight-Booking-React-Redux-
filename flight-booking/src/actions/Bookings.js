f1180efb4f20b318f785e8e4429dc4458037f175
harjeevansingh <harjeevansingh01@gmail.com>
2021-12-07 09:53:22 +0530

    Actions and Reducers added

--- flight-booking/src/actions/Bookings.js
+++ flight-booking/src/actions/Bookings.js
@@ -1,4 +1,5 @@
 /* Import necessary modules */
+import { deleteBookingAPI, fetchFlightIdsAPI, getAllBookingsAPI, submitBookingAPI, updateBookingAPI } from "../utils/api";
 
 export const GET_ALL_BOOKINGS = 'GET_ALL_BOOKINGS';
 export const SET_MESSAGE = 'SET_MESSAGE';
@@ -7,39 +8,91 @@ export const GET_ALL_FLIGHTIDS = 'GET_ALL_FLIGHTIDS'
 // Set Message action creator
 export const setMessageAction = (successMessage, errorMessage) => {
     /* your code goes here */
+    return({
+        type: SET_MESSAGE,
+        successMessage,
+        errorMessage
+    })
 }
 
 // Get bookings action creator
 export const getBookingsAction = (bookings) => {
     /* your code goes here */
+    return({
+        type: GET_ALL_BOOKINGS,
+        bookings
+
+    })
 }
 
 //fetch FlightIds action creator
 export const fetchFlightIdsAction = (flightIds) => {
     /* your code goes here */
+    return ({
+        type:GET_ALL_FLIGHTIDS,
+        flightIds
+    })
 }
 
 // Get Bookings
 export const handleGetBookings = (dispatch) => {
     /* your code goes here */
+    try{
+        const bookings = getAllBookingsAPI();
+        dispatch(getBookingsAction(bookings));
+        dispatch(setMessageAction("", ""));
+    }catch(error){
+        dispatch(setMessageAction("", error.message));  // Only error message has to be set and success msg shouls not have been toucheds(clear- mentioned only msg should be displayed)
+        dispatch(getBookingsAction([]))
+    }
+    
+
 }
 
 //Delete Booking
-export const handleDeleteBooking = (dispatch, bookingId) => {
+export const handleDeleteBooking = (dispatch, bookingId) => {    // from are they getting the dispatch 
     /* your code goes here */
+    try{
+        const successMessage = deleteBookingAPI(bookingId);
+        dispatch(setMessageAction(successMessage, ""));
+        handleGetBookings(dispatch);
+    }catch(error){
+        dispatch(setMessageAction("", error.message));
+    }
+
 }
 
 //Update Booking
 export const handleUpdateBooking = (dispatch, bookingId, formData) => {
     /* your code goes here */
+    try{
+        const successMessage = updateBookingAPI(bookingId, formData);
+        dispatch(setMessageAction(successMessage, ""));
+    }catch(error){
+        dispatch(setMessageAction("", error.message));
+    }
 }
 
 //Fetch all flight Ids
 export const handleFetchFlightIds = (dispatch) => {
     /* your code goes here */
+    try{
+        const flightIds = fetchFlightIdsAPI();
+        dispatch(fetchFlightIdsAction(flightIds));
+        dispatch(setMessageAction("", ""));
+    }catch(error){
+        dispatch(setMessageAction("", error.message));
+        dispatch(fetchFlightIdsAction([]));
+    }
 }
 
 //Submit Booking
 export const handleSubmitBooking = (dispatch, formData) => {
     /* your code goes here */
+    try{
+        const successMessage = submitBookingAPI(formData);
+        dispatch(setMessageAction(successMessage, ""));
+    }catch(error){
+        dispatch(setMessageAction("", error.message));
+    }
 }
--- flight-booking/src/reducers/Bookings.js
+++ flight-booking/src/reducers/Bookings.js
@@ -1,16 +1,39 @@
-// import { GET_ALL_BOOKINGS,  SET_MESSAGE, GET_ALL_FLIGHTIDS } from "../actions/Bookings";
+import { GET_ALL_BOOKINGS,  SET_MESSAGE, GET_ALL_FLIGHTIDS } from "../actions/Bookings";
 
 //Return all flight Ids
 export const FlightIds = ( state = [], action ) => {
     /* your code goes here */
+    if(action.type == GET_ALL_FLIGHTIDS){  // may need to use equals
+        return([                                // might need to remove the ...state
+            ...state,
+            action.flightIds
+        ]);
+    }else{
+        return [...state]
+    }
 }
 
 //Returns all bookings
 export const AllBookings = (state = null, action) => {
     /* your code goes here */
+    if(action.type== GET_ALL_BOOKINGS){
+        return action.bookings // might need to replace bookings by flightIds
+    }else{
+        return state;
+    }
 }
 
 //Return success and error messages
 export const Messages = (state = { successMessage: "", errorMessage: '' }, action) => {
     /* your code goes here */
+    if(action.type== SET_MESSAGE){
+        return ({
+            ...state,
+            successMessage:action.successMessage,
+            errorMessage:action.errorMessage
+        })
+    }else{
+        return {...state}
+    }
+
 }
