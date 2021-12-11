26060762146d111eba89394c1a67414fe4709e4e
harjeevansingh <harjeevansingh01@gmail.com>
2021-12-11 22:35:27 +0530

    Added update booking component

--- flight-booking/src/components/updateBooking.js
+++ flight-booking/src/components/updateBooking.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { handleUpdateBooking } from "../actions/Bookings";
 /* Import necessary modules here */
 
 class UpdateBooking extends Component {
@@ -23,22 +24,51 @@ class UpdateBooking extends Component {
         };
     }
 
+    componentDidMount = ()=>{
+        this.setState({form:{...this.state.form, bookingId:this.props.match.params.bookingId}});
+    }
+
 
 
     updateBooking = () => {
         /* your code goes here */
+        handleUpdateBooking(this.props.dispatch, this.state.id, this.state.form);   // might need to handle error message
     }
 
     handleSubmit = (event) => {
         /* your code goes here */
+        event.preventDefault();
+        this.updateBooking();
     }
 
     handleChange = (event) => {
         /* your code goes here */
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({form:{...this.state.form, [name]:value}});
+        this.validateField(name,value);
     }
 
     validateField = (fieldName, value) => {
-        /* your code goes here */
+        /* your code goes here */        
+        var {formErrorMessage, formValid} = this.state;
+        switch(fieldName){
+            case "noOfTickets":
+                if(value===""){  // Check another comparison method or operator
+                    formErrorMessage[fieldName] = "field required";
+                    formValid[fieldName] = false;
+                }else if(value<1 || value>=10){
+                formErrorMessage[fieldName] = "No of tickets should be greater than 0 and less than 10";
+                formValid[fieldName] = false;
+                console.log(formErrorMessage);
+                } else{
+                formErrorMessage[fieldName] = "";
+                formValid[fieldName] = true;
+                }
+                break;
+        }
+        formValid["buttonActive"] = formValid.bookingId && formValid.noOfTickets;
+        this.setState({formValid:formValid, formErrorMessage:formErrorMessage});
     }
 
     render() {
@@ -54,6 +84,24 @@ class UpdateBooking extends Component {
                                 </div>
                                 <div className="card-body">
                                     {/* Implement the JSX of UpdateBooking component here */}
+                                        <form className="form" onSubmit={this.handleSubmit}>
+                                            <div className="form-group">
+                                                <label htmlFor="bookingId">BookingId</label>
+                                                <input className="form-control" name="bookingId" disabled type="text" value={this.state.form.bookingId}
+                                                    />
+                                                <span className="text-danger">{this.state.formErrorMessage.bookingId}</span>
+                                            </div>
+                                            <div className="noOfTickets">
+                                                <lable htmlFor="noOfTickets">Number of tickets</lable>
+                                                <input type="number" min="1" max="10" value={this.state.form.noOfTickets} onChange={this.handleChange} name="noOfTickets"
+                                                    className="form-control" placeholder="min-1 max-10"></input>
+                                                <span className="text-danger">{this.state.formErrorMessage.noOfTickets}</span>
+                                            </div>
+                                            <button type="submit" className="btn btn-primary" disabled={!this.state.formValid.buttonActive}>Update Booking</button>
+                                        </form>
+                                        <span className="text-danger">{this.props.Messages.errorMessage}</span>
+                                        <span className="text-success">{this.props.Messages.successMessage}</span>
+
                                 </div>
                             </div>
                         </div>
@@ -65,4 +113,9 @@ class UpdateBooking extends Component {
 }
 
 //Pass state props to this component and export it
-export default connect()(UpdateBooking);
+function mapStateToProps(state){
+    return({
+        Messages:state.Messages
+    })
+}
+export default connect(mapStateToProps)(UpdateBooking);
